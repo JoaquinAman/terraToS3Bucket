2f5a6930c3e590b298d268cea1599fc0422ad3c9
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-07-10 18:46:41 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_ecs_cluster" "cluster" {
-  name = "${var.project}-${var.environment}"
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+# resource "aws_ecs_cluster" "cluster" {
+#   name = "${var.project}-${var.environment}"
+#   setting {
+#     name  = "containerInsights"
+#     value = "enabled"
+#   }
+#   capacity_providers = ["FARGATE", ]
+# }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "VPCTerraformjoaquin"
   }
-  capacity_providers = ["FARGATE", ]
 }
