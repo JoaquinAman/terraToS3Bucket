c4329ed0330d2577053e92bfe760cb8640cf9ed6
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-07-11 22:46:44 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,30 +15,3 @@ resource "aws_vpc" "main" {
     Name = "VPCTerraformjoaquin"
   }
 }
-
-# resource "aws_cloudformation_slack" "network" {
-#   name = "networking-stack"
-  
-#   parameteners = {}
-#   capabilities = ["CAPABILITY_IAM"]
-  
-#   template_body = <<STACK
-# {
-#     "Parameters": {},
-#     "Resources": [
-#       "LB14H9U": {
-#         "Type": "AWS::Lex::Bot",
-#         "Properties": {
-#           "Name": "MegamindFromTerraform",
-#           "RoleArn":"arn:aws:iam::077492956248:role/lambda-invoke-role",
-#           "DataPrivacy": {
-#             "ChildDirected": false
-#           },
-#           "idleSessionTTLInSeconds": 300,
-#           "Description": "Basic bot created from terraform"
-#         }
-#       }
-#     }
-#   }
-#   STACK
-# }
