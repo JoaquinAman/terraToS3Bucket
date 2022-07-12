f01e9c3393532c98868d3fe40c3993c044703459
JoaquinAman <55667385+JoaquinAman@users.noreply.github.com>
2022-07-11 22:24:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,38 +7,38 @@
 #   capacity_providers = ["FARGATE", ]
 # }
 
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = "VPCTerraformjoaquin"
-#   }
-# }
-
-resource "aws_cloudformation_slack" "network" {
-  name = "networking-stack"
-  
-  parameteners = {}
-  capabilities = ["CAPABILITY_IAM"]
-  
-  template_body = <<STACK
-{
-    "Parameters": {},
-    "Resources": [
-      "LB14H9U": {
-        "Type": "AWS::Lex::Bot",
-        "Properties": {
-          "Name": "MegamindFromTerraform",
-          "RoleArn":"arn:aws:iam::077492956248:role/lambda-invoke-role",
-          "DataPrivacy": {
-            "ChildDirected": false
-          },
-          "idleSessionTTLInSeconds": 300,
-          "Description": "Basic bot created from terraform"
-        }
-      }
-    }
+  tags = {
+    Name = "VPCTerraformjoaquin"
   }
-  STACK
 }
+
+# resource "aws_cloudformation_slack" "network" {
+#   name = "networking-stack"
+  
+#   parameteners = {}
+#   capabilities = ["CAPABILITY_IAM"]
+  
+#   template_body = <<STACK
+# {
+#     "Parameters": {},
+#     "Resources": [
+#       "LB14H9U": {
+#         "Type": "AWS::Lex::Bot",
+#         "Properties": {
+#           "Name": "MegamindFromTerraform",
+#           "RoleArn":"arn:aws:iam::077492956248:role/lambda-invoke-role",
+#           "DataPrivacy": {
+#             "ChildDirected": false
+#           },
+#           "idleSessionTTLInSeconds": 300,
+#           "Description": "Basic bot created from terraform"
+#         }
+#       }
+#     }
+#   }
+#   STACK
+# }
